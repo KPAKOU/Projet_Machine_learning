384ccd240366972e90e2717e1bc5e0ed2a4631a2
unknown <kpakoum6@gmail.com>
2024-12-18 13:43:04 +0000

    Version finale

--- appli/run_app.py
+++ appli/run_app.py
@@ -4,7 +4,7 @@ def run_backend():
     subprocess.Popen(["uvicorn", "appli.tpi:app", "--reload"])
 
 def run_frontend():
-    subprocess.Popen(["streamlit", "run", "appli\global.py"])
+    subprocess.Popen(["streamlit", "run", "appli/global.py"])
 
 if __name__ == "__main__":
     run_backend()
