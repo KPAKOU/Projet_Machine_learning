db81282de6db84c042216533f8b9c1b87a5621b0
unknown <kpakoum6@gmail.com>
2024-12-18 21:43:42 +0000

    deployement de l'application

--- appli/run_app.py
+++ appli/run_app.py
@@ -1,10 +1,27 @@
 import subprocess
+import socket
+
+def is_port_in_use(port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex(('localhost', port)) == 0
+
+def find_available_port(initial_port):
+    port = initial_port
+    while is_port_in_use(port):
+        port += 1
+    return port
 
 def run_backend():
-    subprocess.Popen(["uvicorn", "appli.tpi:app", "--reload", "--port", "8001"])
+    initial_port = 8001
+    port = find_available_port(initial_port)
+    print(f"Starting backend on port {port}")
+    subprocess.Popen(["uvicorn", "appli.tpi:app", "--reload", "--port", str(port)])
 
 def run_frontend():
-    subprocess.Popen(["streamlit", "run", "appli/global.py", "--server.port", "8502"])
+    initial_port = 8502
+    port = find_available_port(initial_port)
+    print(f"Starting frontend on port {port}")
+    subprocess.Popen(["streamlit", "run", "appli/global.py", "--server.port", str(port)])
 
 if __name__ == "__main__":
     run_backend()
