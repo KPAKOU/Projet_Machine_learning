b1a256d95f8ce41fd794e7597abdc1b6ce48d3c8
unknown <kpakoum6@gmail.com>
2024-12-18 17:51:55 +0000

    Version finale

--- appli/run_app.py
+++ appli/run_app.py
@@ -1,10 +1,10 @@
 import subprocess
 
 def run_backend():
-    subprocess.Popen(["uvicorn", "appli.tpi:app", "--reload"])
+    subprocess.Popen(["uvicorn", "appli.tpi:app", "--reload", "--port", "8001"])
 
 def run_frontend():
-    subprocess.Popen(["streamlit", "run", "appli/global.py"])
+    subprocess.Popen(["streamlit", "run", "appli/global.py", "--server.port", "8502"])
 
 if __name__ == "__main__":
     run_backend()
